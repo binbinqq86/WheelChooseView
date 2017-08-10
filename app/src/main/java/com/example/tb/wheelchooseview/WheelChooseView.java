e97ce8fcc08a7dd1062ffd449138b9f8974a0d4f
binbinqq86 <624715451@qq.com>
2017-08-10 10:52:31 +0800

    update

--- app/src/main/java/com/example/tb/wheelchooseview/WheelChooseView.java
+++ app/src/main/java/com/example/tb/wheelchooseview/WheelChooseView.java
@@ -33,11 +33,11 @@ public class WheelChooseView extends View {
     /**
      * 当前选中位置，默认选中第一个
      */
-    private int currIndex = 1;
+    private int currIndex = 5;
     /**
      * 最大显示数据个数，默认5个
      */
-    private int maxShowNum = 5;
+    private int maxShowNum = 3;
     /**
      * 是否是循环滚动模式
      */
@@ -172,85 +172,88 @@ public class WheelChooseView extends View {
             return;
         }
         
-        textPaint.setTextSize(centerTextSize);
-        Paint.FontMetrics cFontMetrics = textPaint.getFontMetrics();
-        float centerFontHeight = cFontMetrics.bottom - cFontMetrics.top;
-        float y1 = height / 2f - centerTextPadding - centerFontHeight / 2f;
-        float y2 = height / 2f + centerTextPadding + centerFontHeight / 2f;
-        canvas.drawLine(0, y1, width, y1, linePaint);
-        canvas.drawLine(0, y2, width, y2, linePaint);
-        
         int size = dataList.size();
         int center = maxShowNum / 2;
         float eachCenterYPre = 0f;
         float eachCenterYCurr;
         TextPaint textPaintPre = new TextPaint(textPaint);
         for (int i = -center; i <= center; i++) {
+            //开始绘制文本=====================================
+            
+            //区分中间跟边缘文本的颜色
+            if (i == 0) {
+                textPaint.setColor(centerTextColor);
+            } else {
+                textPaint.setColor(otherTextColor);
+            }
+            
+            //计算textPadding缩放等级，上下分别的padding，不是两者的和
+            float tempScalePadding = centerTextPadding - Math.abs(i) * (centerTextPadding - centerTextPadding / scaleTextPadding) / center;
+            //计算textSize缩放等级
+            float tempScaleSize = centerTextSize - Math.abs(i) * (centerTextSize - centerTextSize / scaleTextSize) / center;
+            //计算textAlpha缩放等级
+            float tempScaleAlpha = 255f - Math.abs(i) * (255f - 255f / scaleTextAlpha) / center;
+            //计算绕x轴旋转的角度
+            float tempScaleRotateDegree = -DEFAULT_MAX_ROTATE_DEGREE * i / center;
+//            Log.e(TAG, "onDraw: " + tempScaleAlpha + "$" + tempScalePadding + "$" + tempScaleSize + "$" + tempScaleRotateDegree);
+            textPaint.setTextSize(tempScaleSize);
+            textPaint.setAlpha((int) tempScaleAlpha);
+
+            matrix.reset();
+            camera.save();
+            camera.rotateX(tempScaleRotateDegree);
+            camera.getMatrix(matrix);
+            camera.restore();
+    
+            //修正失真，主要修改 MPERSP_0 和 MPERSP_1
+            float[] mValues = new float[9];
+            matrix.getValues(mValues);//获取数值
+            mValues[6] = mValues[6] / getResources().getDisplayMetrics().density;//数值修正
+            mValues[7] = mValues[7] / getResources().getDisplayMetrics().density;//数值修正
+            matrix.setValues(mValues);//重新赋值
+            
+            // 调节中心点
+            matrix.preTranslate(-centerX, -centerY);
+            matrix.postTranslate(centerX, centerY);
+            
+            canvas.concat(matrix);
+            
+            //=======================计算baseLine===============================
+            float tempScalePaddingPre = centerTextPadding - Math.abs(i - 1) * (centerTextPadding - centerTextPadding / scaleTextPadding) / (maxShowNum / 2);
+            float tempScaleSizePre = centerTextSize - Math.abs(i - 1) * (centerTextSize - centerTextSize / scaleTextSize) / (maxShowNum / 2);
+            textPaintPre.setTextSize(tempScaleSizePre);
+            
+            String text = "";//默认绘制空文本，占位
             int realIndex = i + currIndex;
             if (realIndex >= 0 && realIndex < size) {
-                //开始绘制文本=====================================
-                
-                //区分中间跟边缘文本的颜色
-                if (i == 0) {
-                    textPaint.setColor(centerTextColor);
-                } else {
-                    textPaint.setColor(otherTextColor);
-                }
-                
-                //计算textPadding缩放等级，上下分别的padding，不是两者的和
-                float tempScalePadding = centerTextPadding - Math.abs(i) * (centerTextPadding - centerTextPadding / scaleTextPadding) / center;
-                //计算textSize缩放等级
-                float tempScaleSize = centerTextSize - Math.abs(i) * (centerTextSize - centerTextSize / scaleTextSize) / center;
-                //计算textAlpha缩放等级
-                float tempScaleAlpha = 255f - Math.abs(i) * (255f - 255f / scaleTextAlpha) / center;
-                //计算绕x轴旋转的角度
-                float tempScaleRotateDegree = -DEFAULT_MAX_ROTATE_DEGREE * i / center;
-//                Log.e(TAG, "onDraw: " + tempScaleAlpha + "$" + tempScalePadding + "$" + tempScaleSize + "$" + tempScaleRotateDegree);
-                textPaint.setTextSize(tempScaleSize);
-                textPaint.setAlpha((int) tempScaleAlpha);
-                
-                matrix.reset();
-                camera.save();
-                camera.rotateX(tempScaleRotateDegree);
-                camera.getMatrix(matrix);
-                camera.restore();
-                
-                // 修正失真，主要修改 MPERSP_0 和 MPERSP_1
-                float[] mValues = new float[9];
-                matrix.getValues(mValues);//获取数值
-                mValues[6] = mValues[6] / getResources().getDisplayMetrics().density;//数值修正
-                mValues[7] = mValues[7] / getResources().getDisplayMetrics().density;//数值修正
-                matrix.setValues(mValues);//重新赋值
-                
-                // 调节中心点
-                matrix.preTranslate(-centerX, -centerY);
-                matrix.postTranslate(centerX, centerY);
-
-//                canvas.concat(matrix);
-                
-                //=======================计算baseLine===============================
-                float tempScalePaddingPre = centerTextPadding - Math.abs(i - 1) * (centerTextPadding - centerTextPadding / scaleTextPadding) / (maxShowNum / 2);
-                float tempScaleSizePre = centerTextSize - Math.abs(i - 1) * (centerTextSize - centerTextSize / scaleTextSize) / (maxShowNum / 2);
-                textPaintPre.setTextSize(tempScaleSizePre);
-                
-                String text = dataList.get(realIndex);
-                Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
-                float fontHeight = fontMetrics.bottom - fontMetrics.top;
-                
-                Paint.FontMetrics fontMetricsPre = textPaintPre.getFontMetrics();
-                float fontHeightPre = fontMetricsPre.bottom - fontMetricsPre.top;
-                
-                if (i == -center) {//计算有问题===================
-                    eachCenterYPre = (height - contentHeight) / 2f + tempScalePadding + fontHeight / 2f;
-                    eachCenterYCurr = eachCenterYPre;
-                } else {
-                    eachCenterYCurr = eachCenterYPre + tempScalePaddingPre + fontHeightPre / 2f + tempScalePadding + fontHeight / 2f;
-                    eachCenterYPre = eachCenterYCurr;
-                }
-                //baseLine计算参考：http://blog.csdn.net/harvic880925/article/details/50423762
-                float baseline = eachCenterYCurr + (fontMetrics.bottom - fontMetrics.top) / 2f - fontMetrics.bottom;
-                canvas.drawText(text, width / 2, baseline, textPaint);
+                text = dataList.get(realIndex);
             }
+            Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+            float fontHeight = fontMetrics.bottom - fontMetrics.top;
+            
+            Paint.FontMetrics fontMetricsPre = textPaintPre.getFontMetrics();
+            float fontHeightPre = fontMetricsPre.bottom - fontMetricsPre.top;
+            
+            if (i == -center) {
+                eachCenterYPre = (height - contentHeight) / 2f + tempScalePadding + fontHeight / 2f;
+                eachCenterYCurr = eachCenterYPre;
+            } else {
+                eachCenterYCurr = eachCenterYPre + tempScalePaddingPre + fontHeightPre / 2f + tempScalePadding + fontHeight / 2f;
+                eachCenterYPre = eachCenterYCurr;
+            }
+            //baseLine计算参考：http://blog.csdn.net/harvic880925/article/details/50423762
+            float baseline = eachCenterYCurr + (fontMetrics.bottom - fontMetrics.top) / 2f - fontMetrics.bottom;
+            canvas.drawText(text, width / 2, baseline, textPaint);
+        }
+        //绘制分割线
+        if (hasSeparateLine) {
+            textPaint.setTextSize(centerTextSize);
+            Paint.FontMetrics cFontMetrics = textPaint.getFontMetrics();
+            float centerFontHeight = cFontMetrics.bottom - cFontMetrics.top;
+            float y1 = height / 2f - centerTextPadding - centerFontHeight / 2f;
+            float y2 = height / 2f + centerTextPadding + centerFontHeight / 2f;
+            canvas.drawLine(0, y1, width, y1, linePaint);
+            canvas.drawLine(0, y2, width, y2, linePaint);
         }
     }
     
@@ -266,6 +269,7 @@ public class WheelChooseView extends View {
             Paint.FontMetrics fontMetrics = tp.getFontMetrics();
             contentHeight += fontMetrics.bottom - fontMetrics.top + 2 * tempScalePadding;
         }
+//        Log.e(TAG, "calculateContentHeight: "+contentHeight );
     }
     
     public float getScaleTextPadding() {
